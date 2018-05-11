0757764312748fe631fb3815cd0fd01b1c507c70
Schifer <oneiros241@gmail.com>
2018-05-11 18:07:57 -0300

    cad_fornec scene made

--- ControleEstoque/src/Main.java
+++ ControleEstoque/src/Main.java
@@ -11,6 +11,8 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
 
 
 public class Main extends Application/* implements EventHandler<ActionEvent> */{ // Sem event handler: lambda expressions
@@ -40,8 +42,11 @@ public class Main extends Application/* implements EventHandler<ActionEvent> */{
 
         });
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button_fornec);
+        GridPane layout = new GridPane();
+        layout.add(button_fornec, 0, 0);
+        layout.setMinSize(400, 200);
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        //layout.getChildren().add(button_fornec);
 
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
@@ -49,18 +54,53 @@ public class Main extends Application/* implements EventHandler<ActionEvent> */{
 
         // Layout cad_fornec
         GridPane layout_fornec = new GridPane();
+        layout_fornec.setVgap(5);
+        layout_fornec.setHgap(5);
 
         // botton for cad_fornec
         button_ok = new Button();
         button_ok.setText("Ok");
 
         // Text label for cad_fornec scene
-        Label Label_cad = new Label("Digite as informações do fornecedor");
+        Label Label_cad = new Label("Informações:");
         Label_cad.setTextAlignment(TextAlignment.JUSTIFY);
-        
-        layout_fornec.setPadding(new Insets(20, 20, 20, 20));
-        layout_fornec.getChildren().addAll(Label_cad, button_ok);
-        scene_cad_fornec = new Scene(layout_fornec, 500, 450);
+        Label Label_CNPJ = new Label("CNPJ:");
+        Label_CNPJ.setTextAlignment(TextAlignment.JUSTIFY);
+        Label Label_name = new Label("Nome:");
+        Label_name.setTextAlignment(TextAlignment.JUSTIFY);
+        Label Label_adr = new Label("Endereço:");
+        Label_adr.setTextAlignment(TextAlignment.JUSTIFY);
+        Label Label_phone = new Label("Telefone:");
+        Label_phone.setTextAlignment(TextAlignment.JUSTIFY);
+
+        // Text field inputs for the cad_fornec scene
+        TextField TFCNPJ = new TextField();
+        TFCNPJ.setPromptText("CNPJ");
+        TextField TFName = new TextField();
+        TFName.setPromptText("Nome do fornecedor");
+        TextField TFAdr = new TextField();
+        TFAdr.setPromptText("Endereço");
+        TextField TFPhone = new TextField();
+        TFPhone.setPromptText("Telefone");
+
+
+        // Putting the labels and buttons on the cad_fornec Stage Scene
+        layout_fornec.setAlignment(Pos.CENTER);
+        layout_fornec.add(Label_cad, 0, 0);
+        layout_fornec.add(Label_CNPJ, 0, 1);
+        layout_fornec.add(Label_name, 0, 2);
+        layout_fornec.add(Label_adr, 0, 3);
+        layout_fornec.add(Label_phone, 0, 4);
+        layout_fornec.add(button_ok, 0,5);
+
+
+        // Putting the text fields on the cad_fornec Stage Scene
+        layout_fornec.add(TFCNPJ,1,1);
+        layout_fornec.add(TFName, 1,2);
+        layout_fornec.add(TFAdr, 1, 3);
+        layout_fornec.add(TFPhone,1,4);
+
+        scene_cad_fornec = new Scene(layout_fornec, 300, 250);
         button_ok.setOnAction(e -> {
             // Ok button code for new fornec data
         });
