f65711c1dbd8350e52d97b932f0692a51a198306
Schifer <oneiros241@gmail.com>
2018-05-14 11:32:46 -0300

    classe Ingrediente

---
+++ ControleEstoque/src/Ingrediente.java
@@ -0,0 +1,30 @@
+
+public class Ingrediente {
+    string cod, nome, quantAtual;
+
+    // setters
+    public void setCod(string cod){
+        this.cod = cod;
+    }
+
+    public void setNome(string nome){
+        this.nome = nome;
+    }
+
+    public void setQuantAtual(string quantAtual){
+        this.quantAtual = quantAtual;
+    }
+
+    //getters
+    public string getCod(){
+        return cod;
+    }
+
+    public string getNome(){
+        return nome;
+    }
+
+    public string getQuantAtual(){
+        return quantAtual;
+    }
+}
