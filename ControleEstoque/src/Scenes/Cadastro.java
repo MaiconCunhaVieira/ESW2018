13ef0c8c0dd7957afa83f97b6ed6f08fdc880a1f
MaiconCunhaVieira <maicon_c_v@hotmail.com>
2018-05-18 16:32:17 -0300

    MenuBar added in class Main

--- ControleEstoque/src/Scenes/Cadastro.java
+++ ControleEstoque/src/Scenes/Cadastro.java
@@ -43,8 +43,7 @@ public abstract class Cadastro implements Scenes.Scene {
         textField.setPromptText(text);
         textField.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue,
-                                String newValue) {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 if (!newValue.matches("\\d*")) {
                     textField.setText(newValue.replaceAll("[^\\d]", ""));
                 }
--- ControleEstoque/src/controleestoque/Main.java
+++ ControleEstoque/src/controleestoque/Main.java
@@ -8,17 +8,23 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.collections.ObservableList;
+
+import java.util.List;
 
 
 public class Main extends Application/* implements EventHandler<ActionEvent> */{
     // Sem event handler: lambda expressions
     // lambda expressions = método de fazer as actions do botão de maneiras simples e eficaz.
     public static Stage window;
-    Button button_cad_compra_ingred, button_cad_producao;
+    private GridPane layout;
+    private MenuBar menuBar;
 
     public static void main(String[] args) {
 
@@ -31,31 +37,10 @@ public class Main extends Application/* implements EventHandler<ActionEvent> */{
 
         window = primaryStage;
         window.setTitle("Controle de Estoque");
-        button_cad_compra_ingred = new Button();
-        button_cad_compra_ingred.setText("Cadastrar nova compra de ingrediente");
-        button_cad_compra_ingred.setOnAction(e -> {
+        layout = new GridPane();
 
-            System.out.println("Lambda expression button");
-            System.out.println("Equivale ao void handle...");
-            CadastraCompraIngrediente cadastraCompraIngrediente = new CadastraCompraIngrediente();
-            cadastraCompraIngrediente.ChangeScene();
+        InitializeMenuBar();
 
-        });
-
-        button_cad_producao = new Button();
-        button_cad_producao.setText("Cadastrar nova produção");
-        button_cad_producao.setOnAction(e -> {
-
-            System.out.println("Lambda expression button");
-            System.out.println("Equivale ao void handle...");
-            CadastraProducao cadastraProducao = new CadastraProducao();
-            cadastraProducao.ChangeScene();
-
-        });
-
-        GridPane layout = new GridPane();
-        layout.add(button_cad_compra_ingred, 0, 0);
-        layout.add(button_cad_producao, 0, 1);
         layout.setMinSize(400, 200);
         layout.setPadding(new Insets(10, 10, 10, 10));
         //layout.getChildren().add(button_fornec);
@@ -63,6 +48,34 @@ public class Main extends Application/* implements EventHandler<ActionEvent> */{
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
+    }
+
+    private void InitializeMenuBar(){
+        menuBar = new MenuBar();
+
+        Menu menuCadastrar = new Menu("Cadastrar");
+        Menu menuPesquisar = new Menu("Pesquisar");
+        MenuItem itemCadCompraIngred = new MenuItem("Compra de Ingrediente");
+        MenuItem itemCadProducao = new MenuItem("Produção");
+
+        menuBar.getMenus().addAll(menuCadastrar, menuPesquisar);
+        menuCadastrar.getItems().addAll(itemCadCompraIngred, itemCadProducao);
+
+        itemCadCompraIngred.setOnAction(event -> {
+            System.out.println("Lambda expression button");
+            System.out.println("Equivale ao void handle...");
+            CadastraCompraIngrediente cadastraCompraIngrediente = new CadastraCompraIngrediente();
+            cadastraCompraIngrediente.ChangeScene();
+        });
+
+        itemCadProducao.setOnAction(event -> {
+            System.out.println("Lambda expression button");
+            System.out.println("Equivale ao void handle...");
+            CadastraProducao cadastraProducao = new CadastraProducao();
+            cadastraProducao.ChangeScene();
+        });
+
+        layout.add(menuBar, 0, 0);
     }
 
     /*
